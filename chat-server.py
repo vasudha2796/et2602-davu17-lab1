9bbbcbdc10ce252cbab839f427a592d6f47b9384
vasudha2796 <vasudha.dasari@yahoo.com>
2020-05-31 23:45:48 +0200

    updated

--- chat-server.py
+++ chat-server.py
@@ -1,6 +1,7 @@
 import socket
 import select
 import sys
+import re
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
@@ -46,16 +47,43 @@ def main():
 						if len(msg)>12:
 							users.sendall(' Nicknames should not exceed 12 characters')
 						elif re.search(r'!@#$\' msg)
-                            users.sendall('No special characters in nick names'.encode('utf_8'))
-                        elif find:
-                            users.sendall(('Welcome to chat '+str(msg)).encode('utf_8'))
-                            u_list[users]=msg
-                            user_templist.remove(users)
-                        else:
-                            users.sendall('Enter command NICK <nick>'.encode('utf_8'))
-                except:
-                    continue
-                    
+                            				users.sendall('No special characters in nick names'.encode('utf_8'))
+                        		elif find:
+                            			users.sendall(('Welcome to chat '+str(msg)).encode('utf_8'))
+                            			u_list[users]=msg
+                            			user_templist.remove(users)
+                        		else:
+                            			users.sendall('Enter command NICK <nick>'.encode('utf_8'))
+					else:
+						users.close()
+						userlist.remove(users)
+						user_templist.remove(users)
+                			except:
+                    				continue
+
+				elif users in u_list:
+					try:
+						msg=users.recv(1024).decode('utf_8')
+						if msg:
+							find=re.search(r'@#%/\s',msg)
+							if len(msg)>256:
+								users.sendall('Message should be 256 characters limit'.encode('utf_8'))
+							elif find:
+								msg_s = 'MSG '+str(u_list[users]+': '+msg[4:]
+								broadcast(msg_s, users)
+							else:
+								users.sendall('Enter MSG <msg>'.encode('utf_8'))
+							else:
+								users.close()
+								userlist.remove(users)
+								del u_list[users]
+							except:
+								continue
+				server.close()
+if __name__ == "__main__":
+	main()
+
+                    	
     server.close()
  if __name__ == "__main__":
     main()
