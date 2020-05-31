42137f2511a39cd1ec07b067ed7967370d629f93
vasudha2796 <vasudha.dasari@yahoo.com>
2020-05-31 23:26:09 +0200

    chat application

--- chat-client.py
+++ chat-client.py
@@ -1,1 +1,33 @@
+import socket
+import select
+import sys
+import re
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+	print ("Enter Format: clientscript, IP address:port number, Nick")
+	exit()
+args = str(sys.argv[1].split(':')
+ip_addr = str(args[0])
+port = int(argv[1])
+nick = str(sys.argv[2])
+server.connect((ip_addr, port))
+MESSAGE = server.recv(1024).decode('utf-8')
+print(MESSAGE)
+server.sendall(('NICK '+nick).decode('utf-8'))
+ok = server.recv(1024).decode('utf-8')
+
+while True:
+	sockets_list = [sys.stdin, server]
+	read_sockets,write_sockets, error_sockets = select.select(sockets, [], [])
+	for sock in read_sockets:
+		if sock == server:
+			message = sock.recv(2048).decode('utf-8')
+			message = message[4:]
+			print(message)
+		else:
+			message = sys.stdin.readline()
+			s.sendall(('MESSAGE '+message).encode('utf-8'))
+			
+						
+server.close()
--- chat-server.py
+++ chat-server.py
@@ -1,1 +1,62 @@
+import socket
+import select
+import sys
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+if len(sys.argv) != 2:
+	print("Enter Format: Server Application ipaddr_s:port")
+	sys.exit(1)
+args = str(sys.argv[1].split(':')
+ip_addr = str(args[0])
+port = str(args[1])
+server.bind((ip_addr, port))
+server.listen(100)
+
+userlist = []
+user_templist = []
+u_list = {}
+
+def broadcast(message, conn):
+	for users in userlist:
+		if users != conn:
+			try:
+				users.send(message)
+			except:
+				users.close()
+				userlist.remove(users)
+				
+def main():
+	print("Server connected on", ip_addr, port)
+	while True:
+		readsock,writesock,errorsock=select.select(userlist, [],[])
+		for users in readsock:
+			if users==server:
+				newuser,addr=server.accept()
+				newuser.sendall('Hello'.encode('utf_8'))
+				userlist.append(newuser)
+				user_templist.append(newuser)
+				
+			elif users in user_templist:
+				try:
+					nick=users.recv(1024).decode('utf_8')
+					if nick:
+						find=re.search(r'NICK\s(\S*)', nick)
+						msg=str(find.group(1))
+						if len(msg)>12:
+							users.sendall(' Nicknames should not exceed 12 characters')
+						elif re.search(r'!@#$\' msg)
+                            users.sendall('No special characters in nick names'.encode('utf_8'))
+                        elif find:
+                            users.sendall(('Welcome to chat '+str(msg)).encode('utf_8'))
+                            u_list[users]=msg
+                            user_templist.remove(users)
+                        else:
+                            users.sendall('Enter command NICK <nick>'.encode('utf_8'))
+                except:
+                    continue
+                    
+    server.close()
+ if __name__ == "__main__":
+    main()
+                
